6f82845ef47611605af6b11c8c82cad4b50ad2d9
Saman <saman.yaghini@uoit.net>
2015-10-28 14:54:32 -0400

    Basic outline

---
+++ ArraySort.java
@@ -0,0 +1,12 @@
+public abstract class ArraySort<T> {
+   abstract public void iSort(T[] inArray);
+   abstract public T[] oSort(T[] inArray);
+   public long iSortTimed(T[] inArray) {
+      // TO DO
+
+   }
+   public void setComparator(Comparator<T> comparator) {
+      // TO DO
+
+   }
+}
