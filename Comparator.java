04398e2d7018e6216e23818c421de7b3daaba65e
Saman <saman.yaghini@uoit.net>
2015-10-28 21:27:17 -0400

    Making some progress with generics

--- Comparator.java
+++ Comparator.java
@@ -1,3 +1,3 @@
 public interface Comparator<T> {
-
+   public int compare(T a, T b);
 }
--- sortNumbers.java
+++ sortNumbers.java
@@ -1,6 +1,8 @@
 class BubbleSort<T> extends ArraySort<T> {
+
    public void iSort(T[] inArray) {
-      System.out.println("Hello");
+      IntegerComparator<Integer> myComp = new IntegerComparator<Integer>(true);
+      System.out.println(myComp.compare(1,1));
 
    }
    public T[] oSort(T[] inArray) {
@@ -14,5 +16,35 @@ class BubbleSort<T> extends ArraySort<T> {
 public class sortNumbers {
    public static void main(String[] args) {
       System.out.println("Hello");
+      BubbleSort<Integer> myBub = new BubbleSort<Integer>();
+      Integer[] myarray = new Integer[2];
+      myarray[0] = 1;
+      myarray[1] = 2;
+      myBub.iSort(myarray);
    }
+
+}
+
+class IntegerComparator<T> implements Comparator<T> {
+
+   public int compare(T a, T b) {
+
+      //if (a < b) return -1;
+      if (a == b) {
+         return 0;
+      }
+      else {
+         return 1;
+      }
+      //if (a > b) return 1;
+
+   }
+
+   public IntegerComparator(boolean ascend) {
+      if (ascend == true) {
+
+      }
+
+   }
+
 }
