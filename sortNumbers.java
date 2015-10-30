0a4c037d1f84061b27588fe085fc6468682e450d
Saman <saman.yaghini@uoit.net>
2015-10-30 01:35:36 -0400

    Added arg command-line interperation

--- sortNumbers.java
+++ sortNumbers.java
@@ -1,5 +1,7 @@
+import java.util.Random;
+
 class BubbleSort<T> extends ArraySort<T> {
-   boolean ascend;
+   //boolean ascend;
    Comparator<T> myComp;
    //IntegerComparator myComp = new IntegerComparator(ascend);
    //Comparator<Integer> myComp = new IntegerComparator(ascend);
@@ -23,13 +25,14 @@ class BubbleSort<T> extends ArraySort<T> {
             }
          }
       }
+      System.out.println("Sort complete. Printing :");
       for (int p=0;p<inArray.length;p++) {
-         System.out.print(inArray[p]);
+         System.out.print(inArray[p] + " ");
       }
    }
-   public BubbleSort(boolean ascend) {
+   /*public BubbleSort(boolean ascend) {
       this.ascend = ascend;
-   }
+   }*/
    public T[] oSort(T[] inArray) {
 
       return null; // Placeholder
@@ -39,6 +42,7 @@ class BubbleSort<T> extends ArraySort<T> {
       System.out.println("Calling setComparator ");
 
       //myComp = new IntegerComparator(ascend);
+      myComp = comparator;
 
    }
 
@@ -47,16 +51,71 @@ class BubbleSort<T> extends ArraySort<T> {
 public class sortNumbers {
    public static void main(String[] args) {
       boolean ascend = true;
-      System.out.println("Hello");
-      BubbleSort<Integer> myBub = new BubbleSort<Integer>(ascend);
-      myBub.myComp = new IntegerComparator(ascend); // This works
-      //Integer[] myarray = new Integer[2];
-      //myarray[0] = 1;
-      //myarray[1] = 2;
-      Integer[] myList = {5,3,2,4,1};
-      //myBub.iSort(myarray);
+      Random rand = new Random();
+      int myArrLength = Integer.parseInt(args[0]);
+      String mySortMethod = args[1];
 
-      myBub.iSort(myList);
+      Integer[] myArray = new Integer[myArrLength];
+      switch(mySortMethod) {
+         case "bubble":
+         BubbleSort<Integer> myBub = new BubbleSort<Integer>();
+         Comparator<Integer> myNewComp = new IntegerComparator(ascend);
+         myBub.setComparator(myNewComp);
+         for (int i=0;i<myArrLength;i++) {
+            myArray[i] = rand.nextInt(101);
+
+         }
+         
+         myBub.iSort(myArray);
+         break;
+      }
+
+      String sortDirection;/*
+
+      if (args.length <= 2) {
+         int myArrLength = Integer.parseInt(args[0]);
+         String mySortMethod = args[1];
+         int[] myArray = new int[myArrLength];
+      }
+      else if (args.length <= 3) {
+         int myArrLength = Integer.parseInt(args[0]);
+         String mySortMethod = args[1];
+         sortDirection = args[2];
+
+         int[] myArray = new int[myArrLength];
+
+         if (sortDirection == "ascend") {
+            ascend = true;
+         }
+         else if (sortDirection == "descend") {
+            ascend = false;
+         }
+      }*/
+
+
+      //  Testing block
+      /*BubbleSort<Integer> myBub = new BubbleSort<Integer>(); // Works
+      //myBub.myComp = new IntegerComparator(ascend); // This works
+      Comparator<Integer> myNewComp = new IntegerComparator(ascend); // Also works
+      myBub.setComparator(myNewComp); // Works
+
+      Integer[] myList = {5,3,2,4,1}; // Test list
+
+
+      myBub.iSort(myList);*/
+
+      /* Ignore float, other types for now
+      Float[] myFloatList = {5.1f,3.2f,2.4f,4.1f,1.8f};
+      System.out.println("Floatlist made");
+      BubbleSort<Float> myFloatBub = new BubbleSort<Float>();
+      System.out.println("FloatBub made");
+      Comparator<Float> myFloatComp = new FloatComparator(ascend);
+      System.out.println("floatComp made");
+      System.out.println("Beginning sort");
+      myFloatBub.iSort(myFloatList);
+      */
+
+
 
 
       // use a for loop for args
@@ -96,8 +155,45 @@ class IntegerComparator implements Comparator<Integer> {
 
    public IntegerComparator(boolean ascend) {
       this.ascend = ascend;
+   }
 
+}
 
+class FloatComparator implements Comparator<Float> {
+   boolean ascend;
+
+   public int compare(Float a, Float b) {
+      int floatcomp = Float.compare(a,b);
+      System.out.println("floatcomp = " + floatcomp);
+
+      if (ascend == true) {
+         //System.out.println("Ascend is true");
+         if (floatcomp < 0) {
+            return -1;
+         }
+
+         if (floatcomp > 0) {
+            return 1;
+         }
+         else return 0;
+
+      }
+      else {
+         //System.out.println("Ascend is false");
+         if (floatcomp < 0) {
+            return 1;
+         }
+
+         if (floatcomp > 0) {
+            return -1;
+         }
+         else return 0;
+      }
+
+   }
+
+   public FloatComparator(boolean ascend) {
+      this.ascend = ascend;
    }
 
 }
